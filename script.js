d301ca78ad4a5fcbe8378af88c4add63aa95f7bc
Sawjoshuahtoo98 <joshuahtoo98@gmail.com>
2025-07-31 23:08:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+document.querySelectorAll('section').forEach(sec => {
+  sec.classList.add('hidden');
+  observer.observe(sec);
+});
